54b84ab6ee49efab05c0ba7d409fc10ea62341ff
ceo-2007 <150609055+ceo-2007@users.noreply.github.com>
2026-04-27 09:44:57 +0800

    Add files via upload

---
+++ check-receipt-app-github-ready/service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'check-receipt-app-cache-v1';
+const assets = [
+  './',
+  'index.html',
+  'manifest.webmanifest',
+  'icons/icon-192.png',
+  'icons/icon-512.png',
+  'icons/icon-96.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
